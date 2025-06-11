5f3efee180221e1faf24d68820aa8cb3fb8fc258
Himesh Thakur <thakurhimesh546@gmail.com>
2025-06-11 20:14:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//let menu = document.querySelector('link');
+function item(){
+    document.getElementById("menu").classList.toggle("active");
+
+}
+
+
+function openLogin() {
+    document.getElementById("loginBox").style.display = "block";
+  }
+  //function openLogin() {
+    //document.getElementById("loginBox").style.display = "block";
+ // }
+ function closeLogin() {
+    document.getElementById("loginBox").style.display = "none";
+  }
